8e99bbc431c75bcc78f0ae268e3992418a62dace
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-03-14 23:18:03 -0500

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const rp = require('request-promise');
+const argv = require('minimist')(process.argv.slice(2));
+
+const CLI_ARGS = {
+    iconName: argv.i,
+    color: argv.c,
+}
+
+const getUrl = (name) => `https://simpleicons.org/icons/${name}.svg`;
+
+rp({
+    uri: getUrl(CLI_ARGS.iconName),
+})
+.then(response => {
+    const coloredSvg = response.replace(/(circle|ellipse|path)/g, `$1 fill="${CLI_ARGS.color}"`);
+    fs.writeFile(`${CLI_ARGS.iconName}.svg`, coloredSvg, (err, done) => {
+        process.exit();
+    });
+});
