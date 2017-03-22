cc84ef9b57b80f073cfa90af0c55376fe5b12c19
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-03-22 16:32:26 -0500

    add preview mode, bit of refactoring

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 const fs = require("fs");
 const request = require("request");
 const rp = require("request-promise");
-const chalk = require('chalk');
-const argv = require("minimist")(process.argv.slice(2));
+const chalk = require("chalk");
+const open = require("open");
 const COLORS = require("open-color");
-
+const argv = require("minimist")(process.argv.slice(2));
 const log = console.log;
 
 if (!argv._.length > 0) {
@@ -15,7 +15,15 @@ if (!argv._.length > 0) {
   process.exit(1);
 }
 
-const handleColor = str => {
+const ARGS = {
+  iconName: argv._[0],
+  color: argv.c,
+  preview: !!argv.p || !!argv.preview
+};
+
+const getUrl = name => `https://simpleicons.org/icons/${name}.svg`;
+
+function handleColor(str) {
   const [color, number] = str.split(/(\d+)/).filter(Boolean);
   let hex = COLORS[color];
   if (!hex) return color;
@@ -30,24 +38,34 @@ const handleColor = str => {
   }
 
   return hex;
+}
+
+if (ARGS.preview) {
+  const url = getUrl(ARGS.iconName);
+  open(url);
+
+  log(chalk.green(`Opening ${ARGS.iconName}.svg in your browser...`));
+  process.exit(0);
+}
+
+const getIcon = (uri, iconName, iconColor) => {
+  rp({
+    uri
+  })
+    .then(response => {
+      const coloredSvg = response.replace(
+        /(circle|ellipse|path)/g,
+        `$1 fill="${iconColor}"`
+      );
+
+      fs.writeFile(`${iconName}.svg`, coloredSvg, (err, done) => {
+        log(chalk.green("Done!"));
+        process.exit();
+      });
+    })
+    .catch(error => {
+      log(chalk.red("Ooops!", error));
+    });
 };
 
-const getUrl = name => `https://simpleicons.org/icons/${name}.svg`;
-
-const CLI_ARGS = {
-  iconName: argv._[0],
-  color: handleColor(argv.c),
-};
-
-rp({
-  uri: getUrl(CLI_ARGS.iconName)
-}).then(response => {
-  const coloredSvg = response.replace(
-    /(circle|ellipse|path)/g,
-    `$1 fill="${CLI_ARGS.color}"`
-  );
-  fs.writeFile(`${CLI_ARGS.iconName}.svg`, coloredSvg, (err, done) => {
-    log(chalk.green("Done!"));
-    process.exit();
-  });
-});
+getIcon(getUrl(ARGS.iconName), ARGS.iconName, handleColor(ARGS.color));
