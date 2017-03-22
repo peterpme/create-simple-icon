66464ab9dec37b6ec93f159d6b405fa37cc59e76
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-03-22 11:59:24 -0500

    add open-color palette

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const request = require('request');
-const rp = require('request-promise');
-const argv = require('minimist')(process.argv.slice(2));
+const fs = require("fs");
+const request = require("request");
+const rp = require("request-promise");
+const argv = require("minimist")(process.argv.slice(2));
+const COLORS = require("open-color");
 
 if (!argv._.length > 0) {
-    console.log('Please pass in an icon name. Eg: reddit');
-    process.exit(1);
+  console.log("Please pass in an icon name. Eg: reddit");
+  process.exit(1);
 }
+
+const handleColor = str => {
+  const [color, number] = str.split(/(\d+)/).filter(Boolean);
+  let hex = COLORS[color];
+  if (!hex) return color;
+
+  if (hex) {
+    hex = hex[number || 0];
+  }
+
+  return hex;
+};
+
+const getUrl = name => `https://simpleicons.org/icons/${name}.svg`;
 
 const CLI_ARGS = {
-    iconName: argv._[0],
-    color: argv.c,
-}
-
-const getUrl = (name) => `https://simpleicons.org/icons/${name}.svg`;
+  iconName: argv._[0],
+  color: handleColor(argv.c),
+};
 
 rp({
-    uri: getUrl(CLI_ARGS.iconName),
-})
-.then(response => {
-    const coloredSvg = response.replace(/(circle|ellipse|path)/g, `$1 fill="${CLI_ARGS.color}"`);
-    fs.writeFile(`${CLI_ARGS.iconName}.svg`, coloredSvg, (err, done) => {
-        process.exit();
-    });
+  uri: getUrl(CLI_ARGS.iconName)
+}).then(response => {
+  const coloredSvg = response.replace(
+    /(circle|ellipse|path)/g,
+    `$1 fill="${CLI_ARGS.color}"`
+  );
+  fs.writeFile(`${CLI_ARGS.iconName}.svg`, coloredSvg, (err, done) => {
+    process.exit();
+  });
 });
